8b5274c4033a60a69854d9e6f336a3642c101592
David Zhang <zdavid0123@gmail.com>
2019-09-05 19:18:33 -0400

    add post_pca.sh

---
+++ post_pca.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+root=$1
+refpref=$2
+stupref=$3
+studir=${stupref}_parts100
+methods='sp ap oadp'
+predpopu="python $HOME/fraposa/predstupopu.py"
+home=`pwd`
+
+cd $root
+for method in $methods; do
+    cat $studir/*_$method.pcs > ${stupref}_$method.pcs
+    $predpopu $refpref ${stupref}_$method
+    paste <( cut -f1 ${stupref}_$method.popu ) <( cut -f2- ${stupref}_$method.pcs ) > tmp
+    mv tmp ${stupref}_$method.pcs
+done
+
+cd $home
+for method in $methods; do
+    Rscript plot.R $root/$refpref $root/${stupref}_$method
+done
