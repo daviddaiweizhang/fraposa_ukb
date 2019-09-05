f9911b5cf8e98947d03cbcc370b2872aa7c14e91
David Zhang <zdavid0123@gmail.com>
2019-09-05 19:13:44 -0400

    rename files

--- pre_run.sh
+++ pre_run.sh
@@ -6,5 +6,6 @@ home=`pwd`
 mkdir -p $root
 
 # bash intersect.sh $root
-bash pca_global_all.sh $root
+bash pre_pca_global_all.sh $root
+# bash pre_pca_EUR_oadpEUR.sh $root
 # bash pca_nEUR_small.sh $root $home
