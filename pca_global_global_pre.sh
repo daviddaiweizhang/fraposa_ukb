d2c48a73c65be6cc5cf9e426fbe79c2bd2f484c4
David Zhang <zdavid0123@gmail.com>
2019-09-11 15:10:20 -0400

    quote scriptargs

--- pca_global_global_pre.sh
+++ pca_global_global_pre.sh
@@ -4,8 +4,8 @@ set -e
 root=$1
 refpref=kgn
 inpref=ukb
-scriptargs="pca.slurm $root $refpref $inpref $n"
 n=100
+scriptargs="pca.slurm $root $refpref $inpref $n"
 jobname=global_all_parts${n}
 
-bash submitjobs.sh $scriptargs $jobname $n
+bash submitjobs.sh "$scriptargs" $jobname $n
