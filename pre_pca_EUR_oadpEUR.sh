fe08a28e2cd1ba5e022753c08dcb0cf7b3ec5f0b
David Zhang <zdavid0123@gmail.com>
2019-09-11 12:00:17 -0400

    move overlapping codes to submitjobs.sh

--- pre_pca_EUR_oadpEUR.sh
+++ pre_pca_EUR_oadpEUR.sh
@@ -24,7 +24,6 @@ cat ${stupref}_$method.pcs | cut -f1 | paste - ${stupref}.fam | awk -v popu=$pop
 plink --bfile ${stupref} --keep-allele-order --keep ${stupref}_$method$popu.fiid --make-bed --out ${stupref}_$method$popu
 
 cd $home
+scriptargs="pca.slurm $root ${refpref}_$popu ${stupref}_$method$popu $n"
 jobname=EUR_oadpEUR_parts${n}
-output_dir=logs/${jobname}
-mkdir -p $output_dir
-sbatch --job-name=$jobname --array=1-$n --time=0-02:00:00 --nodes=2 --mem=8000 --output=$output_dir/%A_%4a.log pca.slurm $root ${refpref}_$popu ${stupref}_$method$popu $n
+bash submitjobs $scriptargs $jobname $n
--- pre_pca_global_all.sh
+++ pre_pca_global_all.sh
@@ -4,9 +4,8 @@ set -e
 root=$1
 refpref=kgn
 inpref=ukb
+scriptargs="pca.slurm $root $refpref $inpref $n"
 n=100
-
 jobname=global_all_parts${n}
-output_dir=logs/${jobname}
-mkdir -p $output_dir
-sbatch --job-name=$jobname --array=1-$n --time=0-01:00:00 --nodes=1 --mem=8000 --output=$output_dir/%A_%4a.log pca.slurm $root $refpref $inpref $n
+
+bash submitjobs.sh $scriptargs $jobname $n
