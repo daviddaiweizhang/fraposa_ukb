8968c13c7e0f5d5c2415e26fef8d6dca2e2b3419
David Zhang <zdavid0123@gmail.com>
2019-09-11 11:57:05 -0400

    add submitjobs.sh

---
+++ submitjobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+scriptargs=$1
+jobname=$2
+n=$3
+
+output_dir=logs/${jobname}
+mkdir -p $output_dir
+sbatch --job-name=$jobname --array=1-$n --time=0-02:00:00 --nodes=1 --mem=8000 --output=$output_dir/%A_%4a.log $scriptargs
