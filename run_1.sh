e8c8dad1e54833e3436a2e9910702c4fa89ab98b
David Zhang <zdavid0123@gmail.com>
2019-09-22 17:07:14 -0400

    run pca on all the populations

--- run_1.sh
+++ run_1.sh
@@ -6,4 +6,8 @@ home=`pwd`
 mkdir -p $root
 
 bash pca_post.sh $root kgn ukb
-bash pca_EUR_oadpEUR_pre.sh $root
+bash pca_POPU_oadpPOPU.sh $root EUR
+bash pca_POPU_oadpPOPU.sh $root AFR
+bash pca_POPU_oadpPOPU.sh $root AMR
+bash pca_POPU_oadpPOPU.sh $root EAS
+bash pca_POPU_oadpPOPU.sh $root SAS
