8ed4ec4f848765d44ea06c2350434911778ae271
David Zhang <zdavid0123@gmail.com>
2019-09-24 00:17:24 -0400

    do reference pca before submitting jobs

--- pca_global_global_pre.sh
+++ pca_global_global_pre.sh
@@ -7,6 +7,16 @@ inpref=ukb
 n=100
 methods="ap sp oadp"
 
+fraposa="python $HOME/fraposa/fraposa_runner.py"
+dim_ref=4
+dim_spikes=$dim_ref
+methods='sp ap oadp'
+
+# reference pca
+for method in $methods; do
+    $fraposa --method $method --dim_ref $dim_ref --dim_spikes $dim_spikes $refpref
+done
+
 for method in methods; do
     scriptargs="pca.slurm $root $refpref $inpref $method $n"
     jobname=global_global_parts${n}_${method}
