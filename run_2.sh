5c403eb40d223c4ec1c9f8ef4bdd6ae27e2feabe
David Zhang <zdavid0123@gmail.com>
2019-10-01 10:43:52 -0400

    add post pca for AFR and SAS

--- run_2.sh
+++ run_2.sh
@@ -5,5 +5,8 @@ root=data
 home=`pwd`
 mkdir -p $root
 
-bash pca_post.sh $root kgn_EUR ukb_oadpEUR
-bash pca_EUR_oadpEURsmall.sh $root $home
+# bash pca_post.sh $root kgn_EUR ukb_oadpEUR
+# bash pca_EUR_oadpEURsmall.sh $root $home
+
+bash pca_post.sh $root kgn_AFR ukb_oadpAFR
+bash pca_post.sh $root kgn_SAS ukb_oadpSAS
