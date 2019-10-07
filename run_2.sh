7f9860c714d8e2fe12d1c74c9419f8831f5b6a47
David Zhang <zdavid0123@gmail.com>
2019-10-07 15:01:12 -0400

    reorder commands

--- run_2.sh
+++ run_2.sh
@@ -6,7 +6,7 @@ home=`pwd`
 mkdir -p $root
 
 bash pca_post.sh $root kgn_EUR ukb_oadpEUR
-bash pca_EUR_oadpEURsmall.sh $root $home
-
 bash pca_post.sh $root kgn_AFR ukb_oadpAFR
 bash pca_post.sh $root kgn_SAS ukb_oadpSAS
+
+bash pca_EUR_oadpEURsmall.sh $root $home
