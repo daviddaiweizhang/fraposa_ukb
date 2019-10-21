f6765a70898f4e638e50b72ce4be12b04dd37460
David Zhang <contact@thedavidzhang.com>
2019-10-21 13:08:16 -0400

    minor bugs fix

--- pca_global_global_pre.sh
+++ pca_global_global_pre.sh
@@ -14,10 +14,10 @@ methods='sp ap oadp'
 
 # reference pca
 for method in $methods; do
-    $fraposa --method $method --dim_ref $dim_ref --dim_spikes $dim_spikes $refpref
+    $fraposa --method $method --dim_ref $dim_ref --dim_spikes $dim_spikes $root/$refpref
 done
 
-for method in methods; do
+for method in $methods; do
     scriptargs="pca.slurm $root $refpref $inpref $method $n"
     jobname=global_global_parts${n}_${method}
     bash submitjobs.sh "$scriptargs" $jobname $n
