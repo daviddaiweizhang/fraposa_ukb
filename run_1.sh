965b5e66f80cfd8b13e8c20d0ad635fda48727ce
David Zhang <contact@thedavidzhang.com>
2019-10-26 17:19:45 -0400

    add run mix

--- run_1.sh
+++ run_1.sh
@@ -8,6 +8,7 @@ mkdir -p $root
 bash pca_post.sh $root kgn ukb
 bash pca_POPU_oadpPOPU.sh $root EUR
 bash pca_POPU_oadpPOPU.sh $root AFR
+bash pca_POPU_oadpPOPU.sh $root SAS
 bash pca_POPU_oadpPOPU.sh $root AMR
 bash pca_POPU_oadpPOPU.sh $root EAS
-bash pca_POPU_oadpPOPU.sh $root SAS
+bash pca_POPU_oadpPOPU.sh $root mix
