47ab5ca344d663e1c9d4426816dea77f07338d9b
David Zhang <contact@thedavidzhang.com>
2019-10-26 17:19:15 -0400

    set partition to standard

--- submitjobs.sh
+++ submitjobs.sh
@@ -7,4 +7,4 @@ n=$3
 
 output_dir=logs/${jobname}
 mkdir -p $output_dir
-sbatch --job-name=$jobname --array=1-$n --time=0-02:00:00 --nodes=1 --mem=8000 --output=$output_dir/%A_%4a.log $scriptargs
+sbatch --job-name=$jobname --array=1-$n --time=0-02:00:00 --nodes=1 --mem=8000 --output=$output_dir/%A_%4a.log --account=leeshawn --partition=standard $scriptargs
