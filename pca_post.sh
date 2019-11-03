c5b713e8307cd2f4c554b6061b333e40fc7462ca
David Zhang <contact@thedavidzhang.com>
2019-11-02 21:11:35 -0400

    add (optional) adp to pca_post.sh

--- pca_post.sh
+++ pca_post.sh
@@ -5,7 +5,7 @@ root=$1
 refpref=$2
 stupref=$3
 studir=$4
-methods='sp ap oadp'
+methods='sp ap oadp adp'
 predpopu="python $HOME/fraposa/predstupopu.py"
 home=`pwd`
 
@@ -19,16 +19,18 @@ if [ ! -z "$studir" ]; then
 fi
 
 for method in $methods; do
-    $predpopu $refpref ${stupref}_$method
-    if [[ "$stupref" == "ukb" ]]; then
-        awk '{ if ( $2 > 0.875 ) {print $1} else {print "mix"} }' ${stupref}_$method.popu > tmp_popu_$method
-    else
-        cut -f1 ${stupref}_$method.popu > tmp_popu_$method 
+    if [ -f ${stupref}_$method.pcs ]; then
+        $predpopu $refpref ${stupref}_$method
+        if [[ "$stupref" =~ ^(ukb|ukb_small)$ ]]; then
+            awk '{ if ( $2 > 0.875 ) {print $1} else {print "mix"} }' ${stupref}_$method.popu > tmp_popu_$method
+        else
+            cut -f1 ${stupref}_$method.popu > tmp_popu_$method 
+        fi
+        paste <( cat tmp_popu_$method ) <( cut -f2- ${stupref}_$method.pcs ) > tmp
+        mv tmp ${stupref}_$method.pcs
+        cat tmp_popu_$method | sort | uniq -c
     fi
-    paste <( cat tmp_popu_$method ) <( cut -f2- ${stupref}_$method.pcs ) > tmp
-    mv tmp ${stupref}_$method.pcs
 done
-cat tmp_popu_$method | sort | uniq -c
 
 cd $home
 Rscript plot.R $root/$refpref $root/$stupref
