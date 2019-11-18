c86e551e8bd1f0d4de3322ac4a9555130fd7fd77
David Zhang <zdavid0123@gmail.com>
2019-11-18 15:22:29 -0500

    remove comments

--- timer.sh
+++ timer.sh
@@ -7,7 +7,7 @@ nstu=50
 K=8
 k=2
 pref=data/n${n}s${nstu}/a
-# time -p bash fraposa.sh ${pref}_ref ${pref}_stu oadp ${pref}_oadp
+time -p bash fraposa.sh ${pref}_ref ${pref}_stu oadp ${pref}_oadp
 time -p bash fraposa.sh ${pref}_ref ${pref}_stu ap ${pref}_ap
 time -p bash fraposa.sh ${pref}_ref ${pref}_stu sp ${pref}_sp
-# time -p bash trace.sh ${pref}_ref ${pref}_stu $K $k ${pref}_adp ${pref}_ref
+time -p bash trace.sh ${pref}_ref ${pref}_stu $K $k ${pref}_adp ${pref}_ref
