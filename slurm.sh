ea777166698c9cdaad9d6282cb00050ec9efb64e
David Zhang <zdavid0123@gmail.com>
2020-10-23 21:05:29 -0400

    update slurm config for csg

--- slurm.sh
+++ slurm.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=global_globalsmall
-#SBATCH --array=0-19
-#SBATCH --time=12:00:00
+#SBATCH --array=1-1
+#SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8g
 
-#SBATCH --account=leeshawn
-#### #SBATCH --account=jiankang
 #SBATCH --output=logs/%x-%A-%4a.log
-#SBAtCH --partition=standard
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #### #SBATCH --mail-user=daiweiz@umich.edu
@@ -19,8 +16,8 @@ hostname
 echo $SLURM_ARRAY_JOB_ID
 echo $SLURM_ARRAY_TASK_ID
 mkdir -p logs
-module load python R matlab
-source $HOME/env/bin/activate
+source $HOME/venv/bin/activate
+
 date
 
 root=data
