dca41ae127b70be4e18f46f7dc38ddcaced62c33
David Zhang <zdavid0123@gmail.com>
2020-10-23 21:01:57 -0400

    update run_2.sh

--- run_2.sh
+++ run_2.sh
@@ -9,5 +9,6 @@ bash pca_post.sh $root kgn_EUR ukb_oadpEUR
 bash pca_post.sh $root kgn_AFR ukb_oadpAFR
 bash pca_post.sh $root kgn_SAS ukb_oadpSAS
 
-bash pca_globalnEUR_globalsmall.sh $root $home
-bash pca_EUR_oadpEURsmall.sh $root $home
+bash pca_global_globalsmall.sh $root $home 1
+bash pca_EUR_oadpEURsmall.sh $root $home 0
+bash pca_globalnEUR_globalsmall.sh $root $home 1
